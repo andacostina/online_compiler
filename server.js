32342e16fb5f1f48469b8976934c5d61e33a7439
Anda Unghianu <anda.unghianu@yahoo.com>
2018-06-13 13:54:49 +0000

    port 8080 for google cloud vm

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('*', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-var server = app.listen(8000, function() {
+var server = app.listen(8080, function() {
    var host = server.address().address;
    var port = server.address().port;
    console.log("App listening at port " + port);
