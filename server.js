262346ea12f2bf0862ed3eb5c8573a0eb32e8b14
Anda Costina UNGHIANU <aunghianu@bitdefender.com>
2018-06-26 12:23:37 +0300

    fix3

--- server.js
+++ server.js
@@ -131,10 +131,11 @@ var sessionManager = new SessionManager(function(err) {
                 error = error_split.join('\n');
             };
             if (uuid && filename) {
-                exec("mkdir public/" + uuid + " ; cp " + uuid + "/" + filename + " public/" + uuid + "/" + filename);
-                res.json({path: uuid + '/' + filename});
-                res.end();
-                exec("rm -r -f " + uuid);
+                exec("mkdir public/" + uuid + " ; cp " + uuid + "/" + filename + " public/" + uuid + "/" + filename, function(error, stdout, stderr) {
+                    res.json({path: uuid + '/' + filename});
+                    res.end();
+                    exec("rm -r -f " + uuid);
+                });
             }
             else {
                 res.json({path: undefined});
